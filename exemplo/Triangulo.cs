5c4fb80b8a0c0167128a047f234b9c441a9db00f
Mikael Toledo <mikaeltoledo31@gmail.com>
2022-03-21 16:43:13 -0300

    primeiro projeto utilizando classes

---
+++ exemplo/Triangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exemplo{
+    class Triangulo{
+
+        public double A;
+        public double B;
+        public double C;
+
+
+        public double Area(){
+            double P = (A + B + C) / 2.0;
+            return Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+        }
+
+    }
+}
